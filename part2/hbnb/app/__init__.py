01f8ac9100e9eff0a1393d0661770ed86ce3290c
soumaya <soumayabraziii@gmail.com>
2026-03-10 15:27:05 +0000

    fix: register places namespace in create_app

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,20 +1,14 @@
-# hbnb/app/__init__.py
-
-from flask import Flask
 from flask_restx import Api
-
+from .api.v1.place import ns as places_ns
 from .api.v1.users import ns as users_ns
-from .api.v1.places import ns as places_ns
-from .api.v1.reviews import ns as reviews_ns
-from .api.v1.amenities import ns as amenities_ns
+from .api.v1.amenity import ns as amenities_ns
 
 def create_app():
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
-
-    api.add_namespace(users_ns, path="/api/v1/users")
-    api.add_namespace(places_ns, path="/api/v1/places")
-    api.add_namespace(reviews_ns, path="/api/v1/reviews")
-    api.add_namespace(amenities_ns, path="/api/v1/amenities")
-
+    api = Api(app, prefix="/api/v1")
+    
+    api.add_namespace(users_ns)
+    api.add_namespace(places_ns)
+    api.add_namespace(amenities_ns)
+    
     return app
